761ae777460fcfb6bba445e534c9cd97a00c1779
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-01-09 20:47:03 +0000

    adjust_BaseModel_class

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-
 Welcome to the AirBnB clone project!
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Custom base class for the entire project
+Custom BaseModel class for the AirBnb project
 """
 
 from uuid import uuid4
@@ -10,42 +10,49 @@ class BaseModel:
     """Custom base for all the classes in the AirBnb console project
 
     Attributes:
-        id (str): handles unique user identity
-        created_at: assigns current datetime
-        updated_at: updates current datetime
+        id (str): Unique user identity
+        created_at: Time for the creation of the instance
+        updated_at: Last Update of the instance.
 
     Methods:
-        __str__: prints the class name, id, and creates dictionary
-        save(self): updates instance attributes with current datetime
+        __str__: Returns string representation of the instance
+        save(self): updates 'updated_at' with current datetime
         to_dict(self): returns the dictionary values of the instance obj
 
     """
-
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialize a public instance"""
         #convert id value to string
-        self.id = str(uuid4()) 
-        self.created_at = datetime.now()
+        self.id = str(uuid4())
         self.updated_at = datetime.now()
+        self.created_at = datetime.now()
+        # Create dictionary from input arguments
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'updated_at' or key == 'created_at':
+                    # Convert datetime string into obj
+                    # strptime is class method so we call it on datetime
+                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:        
+                    # add items to dict
+                    self.__dict__[key] = value
+
 
     def __str__(self):
         """return string representation of obj"""
-
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
     def save(self):
         """update the updated_at to current time"""
-
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary representation"""
-
         data_copied = self.__dict__.copy()
         #to access name of class
         data_copied["__class__"] = self.__class__.__name__
-    #Convert to ISO format
+        #Convert to ISO format
         data_copied["created_at"] = self.created_at.isoformat()
         data_copied["updated_at"] = self.updated_at.isoformat()
         #return the dictionary
