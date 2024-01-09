6482052a83e6e6e36a17224ffb5806df32525b35
nadashaban10 <nadashaban10@github.com>
2024-01-09 22:49:17 +0200

    CLI

--- console.py
+++ console.py
@@ -6,21 +6,31 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+    def do_EOF(self, line):
+        """Quits command interpreter with ctrl+d
+         Args:
+            line(args): input argument for quiting
+            the terminal
 
-    '''Define the quit command'''
-def do_quit(self):
-    """Quits the program."""
-    print("quitting")
-    return True
+        """
+        return True
+    def do_quit(self, line):
+        """Quit command to exit the program
+         Args:
+            line(args): input argument for quiting
+            the terminal
 
-    '''Define the EOF command'''
-def do_EOF(self, line):
-    """Exits the program when EOF is received."""
-    print()
-    return self.do_quit()
-def emptyline(self):
-    """Do nothing when an empty line is entered."""
-    pass
+        """
+        return True
+    def do_empty_line(self, line):
+        """
+        Eliminates empty lines
+        """
+        pass
+
+
+
+
 if __name__ == '__main__':
     my_cli = HBNBCommand()
     my_cli.cmdloop()
