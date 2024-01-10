9ccbd8cafd8e77a14341362b67e5aafa4a03343f
nadashaban10 <nadashaban10@github.com>
2024-01-10 02:05:31 +0200

    CLI

--- console.py
+++ console.py
@@ -7,6 +7,13 @@ import sys
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+    '''The preloop method 
+    is called once before the command loop starts.
+    '''
+    def preloop(self) -> None:
+        print("Documented commands (type help <topic>):")
+        print("========================================")
+    
     def do_EOF(self, line):
         """Quits command interpreter with ctrl+d
          Args:
@@ -34,10 +41,37 @@ class HBNBCommand(cmd.Cmd):
 
 
 
+''' to handel multiable command
+sys.argv list, which contains 
+all the command-line arguments.
+You can loop through sys.argv[1:] 
+(excluding the first element, which is the script's name)
+and execute the commands
+if __name__ == '__main__':
+     my_cmd = HBNBCommand().cmdloop()
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        my_cmd.onecmd(arg)
+        --------------------- OR----------------
+        if __name__ == "__main__":
+    # not Running in Terminal (non-interactive)
+    if not sys.stdin.isatty():
+        # (onecmd) method is used to interpret a single line of input as a command.
+        for line in sys.stdin:
+            # Read command line by line
+            # Stripe method to remove whitespace from beginning and the end
+            line = line.strip()
+            # Check if the line contains semicolons
+            if ";" in line:
+                # Call the default method
+                HBNBCommand().default(line)
+            else:
+                # Call the onecmd method
+                HBNBCommand().onecmd(line)
+    else:
+        HBNBCommand().cmdloop()
 
-# if __name__ == '__main__':
-#     HBNBCommand().cmdloop()
-    
+'''
     
 
 # Make the interpreter works in a loop
