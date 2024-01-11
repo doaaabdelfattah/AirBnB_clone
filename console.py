47ea16239b201dc1db99d8b431800d63a3f58b94
nadashaban10 <nadashaban10@github.com>
2024-01-11 03:46:26 +0200

    Show command

--- console.py
+++ console.py
@@ -3,46 +3,28 @@
 Custom class for cli program
 """
 import cmd
-import shlex
 import sys
+from models import storage
 from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
-    __classes = {
-        "BaseModel",
-        "User",
-        "Place",
-        "State",
-        "City",
-        "Amenity",
-        "Review"
-    }
+    __classes = {"BaseModel", "User", "State", "City", "Place", "Amenity", "Review"}
     '''The preloop method 
     is called once before the command loop starts.
     '''
     def preloop(self) -> None:
         print("Documented commands (type help <topic>):")
         print("========================================")
-        
-        def do_help(self, line):
-            """overrides help method"""
-        cmd.Cmd.do_help(self, line)
-        
-    def help_quit(self):
-        """ help guide for quit command """
-        print('Quit command to exit the program')
-
-    def help_EOF(self):
-        """ help guide for EOF command """
-        print('EOF command to exit the program')
-
     
     def do_EOF(self, line):
-        """ function to exit the cmd """
-        print()
-        return True
+        """Quits command interpreter with ctrl+d
+         Args:
+            line(args): input argument for quiting
+            the terminal
 
+        """
+        return True
     def do_quit(self, line):
         """Quit command to exit the program
          Args:
@@ -53,7 +35,7 @@ class HBNBCommand(cmd.Cmd):
         return True
     
     # Aliasing (make exit the same as quit)
-    #do_exit = do_quit
+    do_exit = do_quit
     
     def do_empty_line(self, line):
         """ Eliminates empty lines
@@ -67,10 +49,10 @@ class HBNBCommand(cmd.Cmd):
         # Split the arguments to a list
         args = arg.split()
         if not arg:
-            print("** class name missing **")
+            print(" class name missing ")
         else:
             if args[0] not in HBNBCommand.__classes:
-                print("** class doesn't exist **")
+                print(" class doesn't exist ")
             else:
                 # create new object
                 new_obj = eval(args[0])()
@@ -79,19 +61,54 @@ class HBNBCommand(cmd.Cmd):
                 # print its id
                 print(f"{new_obj.id}")
                 
+    def do_show(self, arg):
+        '''
+        Prints the string representation of
+    an instance based on the class name and id
+        '''
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        id_isinstance = args[1]
+        # create the key for the dictionary of stored objects
+        key = "{}.{}".format(class_name, id_isinstance)
+        # get the dictionary of stored objects
+        objects = storage.all()
+        if key not in objects:
+            print("** no instance found **")
+            return
+            # print the value of the key
+        print(objects[key])
+                
+    
+        
+        
+        
+        
+
+        
+
 ''' to handel multiable command
 sys.argv list, which contains 
 all the command-line arguments.
 You can loop through sys.argv[1:] 
 (excluding the first element, which is the script's name)
 and execute the commands
-if __name__ == '__main__':
+if name == 'main':
      my_cmd = HBNBCommand().cmdloop()
 if len(sys.argv) > 1:
     for arg in sys.argv[1:]:
         my_cmd.onecmd(arg)
         --------------------- OR----------------
-        if __name__ == "__main__":
+        if name == "main":
     # not Running in Terminal (non-interactive)
     if not sys.stdin.isatty():
         # (onecmd) method is used to interpret a single line of input as a command.
@@ -113,6 +130,7 @@ if len(sys.argv) > 1:
     
 # Make the interpreter works in a loop
 if __name__ == "__main__":
+
     # not Running in Terminal (non-interactive)
     if not sys.stdin.isatty():
         # (onecmd) method is used to interpret a single line of input as a command.
