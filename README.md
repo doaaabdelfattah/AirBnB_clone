4fef055fd5b6d99b914f92d9367e32f404982ec9
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-01-15 00:13:18 +0200

    “unittest”

--- README.md
+++ README.md
@@ -9,3 +9,26 @@ The Airbnb Command Interpreter is a text-based interface designed to interact
 with the Airbnb Clone project. Users can perform various actions,
 such as searching for accommodations, making bookings, managing listings, and more,
 all through a series of command-line inputs.
+# How to use Console?
+
+```
+$ ./console.py
+(hbnb) help
+
+Documented commands (type help <topic>):
+========================================
+EOF  help  quit
+
+(hbnb) 
+(hbnb) 
+(hbnb) quit
+$
+```
+
+
+# Files and directories: 
+1. models: will contain all classes used for the entire project. A class, called “model” in a OOP project is the representation of an object/instance.
+2. tests
+3. console.py
+4. Authors
+5. ReadMe
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -17,24 +17,30 @@ class TestAmenity_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Amenity class."""
 
     def test_no_args_instantiates(self):
+        ''' '''
         self.assertEqual(Amenity, type(Amenity()))
 
     def test_new_instance_stored_in_objects(self):
+        '''' '''
         self.assertIn(Amenity(), models.storage.all().values())
 
     def test_id_is_public_str(self):
+        ''' '''
         self.assertEqual(str, type(Amenity().id))
 
     def test_created_at_is_public_datetime(self):
+        ''' '''
         self.assertEqual(datetime, type(Amenity().created_at))
 
     def test_name_is_public_class_attribute(self):
+        ''' '''
         am = Amenity()
         self.assertEqual(str, type(Amenity.name))
         self.assertIn("name", dir(Amenity()))
         self.assertNotIn("name", am.__dict__)
 
     def test_two_amenities_unique_ids(self):
+        ''' '''
         am1 = Amenity()
         am2 = Amenity()
         self.assertNotEqual(am1.id, am2.id)
