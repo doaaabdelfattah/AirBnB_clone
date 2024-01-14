e24c5708c6271e014b91b0d31200093ce95bee14
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-01-14 22:11:14 +0200

    docString

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -62,6 +62,7 @@ class TestFileStorage_methods(unittest.TestCase):
         self.assertIn("BaseModel." + instant.id,
                       FileStorage._FileStorage__objects)
 
+    '''unittest for no file'''
     def test_reload_no_file(self):
         self.assertRaises(FileNotFoundError, models.storage.reload())
 
