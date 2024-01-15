1ecd5b578c6041f5b1e0f412df6b7c02ac05844d
nadashaban10 <nadashaban10@github.com>
2024-01-15 04:10:52 +0200

    done

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -78,7 +78,11 @@ class TestFileStorage_methods(unittest.TestCase):
         self.assertIn("BaseModel." + instant.id,
                       FileStorage._FileStorage__objects)
 
-    def test_reload_with_args(self):
+    def test_reload_no_file(self):
+        ''' test reload - nofile'''
+        self.assertTrue(FileNotFoundError, models.storage.reload)
+
+    def test_reload_with_arg(self):
         with self.assertRaises(TypeError):
             models.storage.reload(None)
 
